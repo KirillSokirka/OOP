7d2c3d3979e2eaf3c557b80046cbc75923401cdc
Kirill Sokirka <sokirka2003@gmail.com>
2021-09-15 18:39:59 +0300

    Add comments

--- Lab1/task2.py
+++ Lab1/task2.py
@@ -1,5 +1,6 @@
-import math
 import sys
+
+function_dict = {"sum": "+", "sub": "-", "mult": "*", "div": "/"}
 
 
 def calculate_expression():
@@ -10,47 +11,12 @@ def calculate_expression():
         print("There isn't any expression to calculate")
         return
     try:
-        func, number_of_arguments = get_function(sys.argv[1])
-        if not func:
-            raise Exception("The unknown function")
-        arguments = sys.argv[2::]
-        if len(arguments) != number_of_arguments:
-            raise Exception("The invalid number of arguments")
-        if number_of_arguments == 1:
-            print("Result -> ", func(int(arguments[0])))
-        elif number_of_arguments == 2:
-            print("Result -> ", func(int(arguments[0]), int(arguments[1])))
+        if sys.argv[1] not in function_dict.keys():
+            print("Unknown function")
+            return
+        print("Result -> ", eval(sys.argv[2] + function_dict[sys.argv[1]] + sys.argv[3]))
     except Exception as e:
-        print(e)
-    except ZeroDivisionError as e:
-        print("The error occurred", e)
-
-
-def get_function(func_name):
-    """
-        The function checks the function name and return the needed function
-    :param func_name: the name of function from user
-    :return: lambda function and number of required arguments
-    """
-    if func_name == "add":
-        return lambda x, y: x + y, 2
-    if func_name == "sub":
-        return lambda x, y: x - y, 2
-    if func_name == "mult":
-        return lambda x, y: x * y, 2
-    if func_name == "div":
-        return lambda x, y: x / y, 2
-    if func_name == "mod":
-        return lambda x, y: x % y, 2
-    if func_name == "pow":
-        return lambda x, y: x ** y, 2
-    if func_name == "cos":
-        return lambda x: math.cos(x), 1
-    if func_name == "sin":
-        return lambda x: math.sin(x), 1
-    if func_name == "tan":
-        return lambda x: math.tan(x), 1
-    return None, None
+        print("The error occurred ->", e)
 
 
 calculate_expression()
--- Lab1/task3.py
+++ Lab1/task3.py
@@ -2,20 +2,24 @@ import sys
 
 
 def check_formula(user_input):
+    """
+        Checks the formula`s correctness and if true: return the result
+        of calculation
+    :param user_input: arguments from command line
+    :return: rightness of formula and
+    """
     sign = ['+', '-', '*', '/', '**', '%']
     if user_input[0] in sign or user_input[-1] in sign:
         return False, None
     sign_count = 0
     result = ""
     for el in user_input:
-        if el not in sign and not el.isdigit():
+        if el not in sign and not el.isdigit() or sign_count == 2:
             return False, None
         if el in sign:
             sign_count += 1
         else:
             sign_count = 0
-        if sign_count == 2:
-            return False, None
         result += el
     return True, eval(result)
 
